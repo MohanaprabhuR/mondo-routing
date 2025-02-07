783f6606070e5973aa810e33e93e0067efaf8b22
Mohanaprabhu.R <mohan@timeless.co>
2025-02-07 09:44:44 +0530

    Parrs error fixing

--- src/app/@modal/(.)shows/[id]/page.tsx
+++ src/app/@modal/(.)shows/[id]/page.tsx
@@ -1,16 +1,26 @@
 import ShowModal from "@/components/modal/showmodal";
+interface Show {
+  id: string;
+}
 
-interface PageProps {
+interface ShowResponse {
+  data: Show;
+}
+
+interface InterceptedPageProps {
   params: {
     id: string;
   };
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default async function InterceptedShowPage({ params }: PageProps) {
+export default async function InterceptedShowPage({
+  params,
+}: InterceptedPageProps) {
   const data = await fetch(
     `${process.env.API_URL}/api/shows/${params.id}?populate=*`
   );
-  const show = await data.json();
+  const show: ShowResponse = await data.json();
 
   return <ShowModal show={show.data} />;
 }
