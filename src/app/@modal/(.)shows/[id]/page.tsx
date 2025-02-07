fcedfffdcb240f63e7e9e78fe248fb8b7397a58a
Mohanaprabhu.R <mohan@timeless.co>
2025-02-07 09:48:15 +0530

    Parrs error not fixing fixing

--- src/app/@modal/(.)shows/[id]/page.tsx
+++ src/app/@modal/(.)shows/[id]/page.tsx
@@ -1,22 +1,16 @@
 import ShowModal from "@/components/modal/showmodal";
-interface ShowResponse {
-  data: Show;
-}
-interface Show {
-  id: string;
-}
+
 interface PageProps {
-  params: Promise<{ id: string }>;
-  searchParams?: { [key: string]: string | string[] | undefined };
+  params: {
+    id: string;
+  };
 }
 
 export default async function InterceptedShowPage({ params }: PageProps) {
-  const resolvedParams = await params;
-
   const data = await fetch(
-    `${process.env.API_URL}/api/shows/${resolvedParams.id}?populate=*`
+    `${process.env.API_URL}/api/shows/${params.id}?populate=*`
   );
-  const show: ShowResponse = await data.json();
+  const show = await data.json();
 
   return <ShowModal show={show.data} />;
 }
